85601d2cc838afdd2766bc77ff7f811361a5d6a1
MinShiGee <MinShiGee@gmail.com>
2021-06-20 16:07:28 +0900

    add pmrs-출국심사

---
+++ pmrs-출국심사.py
@@ -0,0 +1,24 @@
+# https://programmers.co.kr/learn/courses/30/lessons/43238
+
+def solution(n, times):
+    
+    answer = 0
+    mx = max(times)
+    
+    l = 0
+    r = (n + 1) * mx
+    
+    while l <= r:
+        mid = (l + r) // 2
+        
+        t = 0
+        for d in times:
+            t += mid // d
+            if t >= n:
+                answer = mid
+                r = mid - 1
+                
+        if t < n:
+            l = mid + 1
+            
+    return answer
