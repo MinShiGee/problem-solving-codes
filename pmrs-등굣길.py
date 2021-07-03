4841e7c789016933c19ad2ce3613e1d8fdd51dae
MinShiGee <MinShiGee@gmail.com>
2021-07-04 01:05:34 +0900

    add pmrs-등굣길.py

---
+++ pmrs-등굣길.py
@@ -0,0 +1,17 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42898
+
+def solution(m, n, puddles):
+    table = [[0 for j in range(m + 1)] for _ in range(n + 1)]
+    water = {}
+    for i, j in puddles:
+        water[(j,i)] = True
+    table[1][0] = 1
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if (i,j) in water:
+                continue
+            table[i][j] = (table[i - 1][j] + table[i][j - 1]) % 1000000007
+
+    answer = table[n][m]
+    return answer
